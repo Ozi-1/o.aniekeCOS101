551d47609b332f2a458b15a331881543cd9c94e5
Ozi-1 <ozioma.anieke@pau.edu.ng>
2025-10-16 00:28:46 +0100

    Add files via upload

---
+++ Week 3/practice_10.rs
@@ -0,0 +1,16 @@
+fn main() {
+	let sum:u8 = 5550 + 7310;    // addition
+	println!("The sum of 5550 and 7310 = {}", sum);
+
+	let difference:f32 = 95.5 - 4.3;   // subtraction
+	println!("The difference of 95.5 and 4.3 = {}", difference);
+
+	let product:u8 = 4 * 30;    //multiplication
+	println!("The multiple of 4 and 30 = {}", product);
+
+	let quotient:f32 = 56.7 / 32.2;    //division
+	println!("The division of 56.7 and 32.2 = {}",quotient);
+
+	let remainder:f32 = 43 % 5;   //remainder 
+	println!("The remainder of 43 and 5 = {}",remainder);
+}

06a9e0ce1c73301fd552001f4d0c104789c8014a
Ozi-1 <ozioma.anieke@pau.edu.ng>
2025-10-16 13:42:49 +0100

    (My second commit)

--- Week 3/practice_3.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-	let result = 10.00;     //f32 by default
-	let interest:f32 = 8.35;
-	let cost:f64 = 15_000.600  //double precision
-
-	println!("result value is {}",result);
-	println!("interest is {}",interest);
-	println!("cost is {}",cost);
-}

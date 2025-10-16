aedcf610179f7e05d13f1234e0fbb6d5d0738f41
Ozi-1 <ozioma.anieke@pau.edu.ng>
2025-10-16 14:31:31 +0100

    (My third commit)

---
+++ week-3/practice_2/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+	let result = 10;    // u8 by default
+	let age:u8 = 20;
+	let sum:i32 = 5 - 15;
+
+	println!("Result value is {} ",result);
+	println!("Age is {} ",age);
+	println!("Sum is {} ",sum);
+	
+}

fba612dac5bace8cbcb4c8e79998d1ce0189b28f
Ozi-1 <ozioma.anieke@pau.edu.ng>
2025-10-22 18:12:46 +0100

    (My fourth commit)

---
+++ week-4/practice_1/target/debug/practice_1.d
@@ -0,0 +1,1 @@
+C:\Users\aaa\OneDrive\Documents\o.aniekeCOS101\week-4\practice_1\target\debug\practice_1.exe: C:\Users\aaa\OneDrive\Documents\o.aniekeCOS101\week-4\practice_1\src\main.rs
---
+++ week-4/practice_8/target/debug/practice_8.d
@@ -0,0 +1,1 @@
+C:\Users\aaa\OneDrive\Documents\o.aniekeCOS101\week-4\practice_8\target\debug\practice_8.exe: C:\Users\aaa\OneDrive\Documents\o.aniekeCOS101\week-4\practice_8\src\main.rs
